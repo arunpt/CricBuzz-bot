3d4263996138ead1e25bee26c2cd737711499bdf
ks406989 <60415968+ks406989@users.noreply.github.com>
2020-09-19 19:10:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,11 +8,11 @@ from handlers.handlers import msg_handlers
 def main():
     logger.warning("ahh waking up...")
     defaults = Defaults(parse_mode=ParseMode.HTML)
-    updater = Updater(token = "YOUR_BOT_TOKEN" ,use_context=True ,workers=200)
+    updater = Updater(token = "1369968432:AAEPQtmPObNfLSQKoJwKqCBFJVneGVTp8No" ,use_context=True ,workers=200)
     msg_handlers(updater.dispatcher)
     updater.start_polling()
     logger.warning("Ready to rock...")
     updater.idle()
 
 if __name__ == '__main__':
-    main()
+    main() 
